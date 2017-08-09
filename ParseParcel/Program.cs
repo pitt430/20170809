a9570ce3a5a17904912e15e7dbc28666c22b6f26
peter.li <peter.li@DEVELOPER20>
2017-08-09 20:54:37 +1200

    Add command line

--- ParseParcel/Program.cs
+++ ParseParcel/Program.cs
@@ -10,6 +10,28 @@ namespace ParseParcel
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Length(mm):");
+                    var length = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Length(mm):");
+                    var breadth = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Length(mm):");
+                    var height = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Weight(kg):");
+                    var weight = Convert.ToInt32(Console.ReadLine());
+
+                    var parcelCostGenerator = new ParcelCostGenerator();
+                    var packageType = parcelCostGenerator.GetParcelCost(new ParcelItem(length, breadth, height, weight));
+                    Console.WriteLine(string.Format("Package Size: {0}, Cost: ${1}", packageType.PackageSize.ToString(), packageType.Cost));
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
         }
     }
 }
